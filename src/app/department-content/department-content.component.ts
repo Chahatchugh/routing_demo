fb4288ffe97af3fa4f8c3d0cbb59f2e29ca72055
Chahat Chugh <chahatchugh1995@gmail.com>
2019-03-13 09:16:06 +0530

    File added

---
+++ src/app/department-content/department-content.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+//import { Router } from '@angular/router' ;
+
+@Component({
+  selector: 'app-department-content',
+  template: `
+    <p>
+      Department List :
+    </p>
+    <ul>
+    <li *ngFor="let department of departments">
+    <span>{{department.list}} </span> {{department.name}}
+    </li>
+    </ul>
+  `,
+  styles: []
+})
+export class DepartmentContentComponent implements OnInit {
+
+  departments = [
+    {list : 1, name: 'Angular'} ,
+    {list : 2, name: 'Node'} ,
+    {list : 3, name: 'MongoDB'} ,
+    {list : 4, name: 'Ruby'} ,
+    {list : 5, name: 'Bootstrap'} ,
+  ] ;
+  Router: any;
+
+ constructor() { } // to access route parameters , we add in constructor
+
+  ngOnInit() {
+    // this.Router.navigate(['/departments/:list', this.departments.list]);
+    /* this.route.paramMap // property  that give all parameters  in route ; type =Observable ;  has method called subscribes and can get value
+    .subscribe(params => {
+      console.log(params);
+      const list = +params.get('list'); // +converts string in number
+      console.log(list);
+    }) ;*/
+  }
+  //onSelect(department) {
+    //this.router.navigate(['/departments', department.list]);
+  //}
+
+}
